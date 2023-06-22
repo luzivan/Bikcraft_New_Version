afe959482c04b2c3c67ecce4c4b0701877d713e6
luzivan <luzivan57@gmail.com>
2023-06-22 19:55:06 -0300

    Js Perguntas Frenquentes

--- js/script.js
+++ js/script.js
@@ -25,4 +25,24 @@ function ativarProduto(parametro) {
 
 }
 
-parametros.forEach(ativarProduto)
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    const ativa = resposta.classList.toggle("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+    
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
